141f93203757cd226996e2f99a24db2a2b432b93
tarun-pacifica <tarun@pacificasearch.com>
2014-11-11 22:37:40 +1100

    added readme file

---
+++ regexforex/converter.rb
@@ -0,0 +1,62 @@
+require 'pry'
+system('clear')
+
+deposit = %w($$22.0000
+GBP44,000
+AUD21k
+EUR18500
+THB10000000.000)
+
+ratios = {'THB'=> 0.035,'$$'=> 1.16,'GBP'=>1.84,'AUD'=>1,'EUR'=>1.44}
+
+puts deposit
+puts ratios
+
+class Forex
+  attr_reader :deposit, :rates
+
+  def initialize(deposit,ratios)
+    @deposit = deposit
+    @ratios = ratios
+    system('clear')
+    cleanse(@deposit,@ratios)
+  end
+
+  private
+# displays running total
+  def puts_subtotal(unit,money,rate,sumtotal)
+     puts "Convert: #{unit}#{money} at #{rate}x ............Running total: AUD#{sumtotal}"
+  end
+#displays grand total at end of loop
+  def puts_grandtotal(sumtotal)
+    2.times { puts }
+    puts "Grand total AUD#{sumtotal}"
+    2.times { puts }
+  end
+
+# apply RegEx methods to money
+  def regexxer(money)
+     money.split(/^[\D|$]{2,3}/).pop
+     money.gsub!(/^[\D|$]{2,3}/,'')
+     money.gsub!(/k$/i,'000')
+     money.gsub!(/,/,'')
+     money.gsub!(/\.0+/,'')
+  end
+
+# cleanses the currency
+# incorporates the other methods
+  def cleanse(deposit, ratios)
+    sumtotal = 0
+    deposit.map do |money|
+     unit = money.match(/^[\D|$]{2,3}/).to_s
+     rate = ratios[unit].to_f
+     regexxer(money)
+     money = ((money.to_f)*rate).round(2)
+     sumtotal = sumtotal + money
+     puts_subtotal(unit,money,rate,sumtotal)
+    end
+    puts_grandtotal(sumtotal)
+  end
+end
+
+binding.pry
---
+++ regexforex/regexforex.md
@@ -0,0 +1,16 @@
+##REGEX/FOREX
+- You have been recently hired as a project consultant for the accounts department at Paypal to automate their manual processes. 
+- Each morning their first task is to tally the overnight currency deposits reported from Paypal offices around the world.
+- Due to a lack of reporting consistency, the deposits are in different formats.
+- Using the sample data below, build a currency converter which will take in a batch of amounts in different formats, convert them into Australian dollars using today's exchange rate and return the total sum in AUD.
+
+*Last nights deposits: 
+$$22.0000
+GBP44,000
+AUD21k
+EUR18500
+THB10000000.000
+
+** you can assign any exchange rate to the currencies.
+
+***$$ = USD (The US office think their currency is pretty superior)
